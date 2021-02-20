58e84ca089f933762bb70eff27fb8e8e1a954506
nilldiggonto <nilldiggonto@gmail.com>
2021-02-20 16:44:00 +0600

    data binding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="myApp">
+    <div class="container mt-4">
+      <div class="row justify-content-md-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header text-center bg-dark text-light">
+              <h4>{{name}}'s LOG LIST</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      name:'Nill'
+    }
   }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color: antiquewhite;
 }
+
 </style>
