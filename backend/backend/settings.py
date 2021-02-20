932c4e9a027d2975c268ae74604180a08dfc878a
nilldiggonto <nilldiggonto@gmail.com>
2021-02-21 01:03:26 +0600

    django cros

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,16 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authapi',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', #cors
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.137.107:8080",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000"
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- frontend/src/views/Posts.vue
+++ frontend/src/views/Posts.vue
@@ -3,45 +3,50 @@
     <Navbar></Navbar>
     <!-- <h4>This is the post page</h4> -->
     <div class="container-fluid">
-      <div v-for="posts in APIData" :key="posts.id">
-        <div class="card border-primary mb-3 mt-2" style="max-width: 20rem">
-          <div class="card-header">{{posts.title}}}}</div>
-          <div class="card-body">
-            <h4 class="card-title">Primary card title</h4>
-            <p class="card-text">
-              {{posts.content}}
-            </p>
+      <div class="row">
+      <!-- <div > -->
+        <div class="col-md-4" v-for="posts in APIData" :key="posts.id">
+          <div class="card border-primary mb-3 mt-2">
+            <div class="card-header">{{ posts.title }}</div>
+            <div class="card-body">
+              <!-- <h4 class="card-title">Primary card title</h4> -->
+              <p class="card-text">
+                {{ posts.content }}
+              </p>
+            </div>
           </div>
-        </div>
+        <!-- </div> -->
+      </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {getAPI} from '../axios-api'
+import { getAPI } from "../axios-api";
 import Navbar from "../components/Navbar";
 export default {
   name: "Posts",
-  data(){
-      return{
-          APIData: []
-      }
+  data() {
+    return {
+      APIData: [],
+    };
   },
 
   components: {
     Navbar,
   },
-  created(){
-      getAPI.get('/posts/',)
-      .then(response=>{
-          console.log('DATA received')
-          this.APIData = response.data
+  created() {
+    getAPI
+      .get("")
+      .then((response) => {
+        console.log("DATA received");
+        this.APIData = response.data;
       })
-      .catch(err=>{
-          console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 };
 </script>
 
