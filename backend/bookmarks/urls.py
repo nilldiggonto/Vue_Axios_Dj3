cd20c7392c8c9953892490896f0b6d8336330e25
nilldiggonto <nilldiggonto@gmail.com>
2021-02-21 02:08:03 +0600

    default signup

--- backend/bookmarks/urls.py
+++ backend/bookmarks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import bookmarkView
+from .views import bookmarkView,SignupView
 urlpatterns = [
     path('',bookmarkView,name='bookmark-page'),
+    path('signup/',SignupView,name='bookmark-signup'),
 ]
--- backend/bookmarks/views.py
+++ backend/bookmarks/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def bookmarkView(request):
     template_name = 'bookmarks/bookmark.html'
     return render(request,template_name)
+
+## Default Signup
+def SignupView(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+
+            return redirect('bookmark-page')
+        else:
+            form = UserCreationForm()
+
+    template_name = 'bookmarks/signup.html'
+    context = {
+        'form':form,
+    }
+    return render(request,template_name,context)
