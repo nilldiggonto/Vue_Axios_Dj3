5ca47be6cba5b586c7fa6313d9796d4c5c51fb44
nilldiggonto <nilldiggonto@gmail.com>
2021-02-21 02:46:26 +0600

    static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,3 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'backend/my_static']
+
+STATIC_ROOT = Path(BASE_DIR,'static/')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,7 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path,include
 
@@ -9,3 +12,7 @@ urlpatterns = [
     path('bookmark/',include('bookmarks.urls')),
     path('dashboard/',include('dashboard.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root= settings.STATIC_ROOT)
+
