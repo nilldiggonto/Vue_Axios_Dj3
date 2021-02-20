202a27a31d85fe49e6f94aa1ae1c95b2ffc1a382
nilldiggonto <nilldiggonto@gmail.com>
2021-02-21 02:43:54 +0600

    Category view

--- backend/bookmarks/views.py
+++ backend/bookmarks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -28,3 +29,13 @@ def SignupView(request):
         'form':form,
     }
     return render(request,template_name,context)
+
+
+
+
+
+####### BOOKMARK CATEGORIES
+@login_required
+def categoriesView(request):
+    template_name = 'bookmarks/categories.html'
+    return render(request,template_name)
