735383e39dec7f8cea66219994aa2c352729f99a
nilldiggonto <nilldiggonto@gmail.com>
2021-02-23 00:36:04 +0600

    Task Model

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title   = models.CharField(max_length=120)
+    date    = models.DateTimeField(auto_now_add=True)
+    complated   = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['complated','date']
+
+    def __str__(self):
+        return self.title
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
 def taskHome(request):
     return render(request,'task/task.html')
+
+
+# class TaskView(View):
+
+#     def get(self)
